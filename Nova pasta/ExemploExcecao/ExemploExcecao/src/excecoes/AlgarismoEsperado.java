5d4503e1475aa765549d9b6a1013735244181978
Karolayne <karolayneamabile@gmail.com>
2025-03-14 23:08:36 -0300

    up

--- Nova pasta/ExemploExcecao/ExemploExcecao/src/excecoes/AlgarismoEsperado.java
+++
@@ -1,9 +0,0 @@
-package excecoes;
-
-public class AlgarismoEsperado extends Exception
-{ 
-	public AlgarismoEsperado (char c){
-		super ("Caractere digitado: " + c + "\n" +
-            "Era esperado um algarismo."); 
-	}
-}
--- Nova pasta/ExemploExcecao/ExemploExcecao/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module ExemploExcecao {
-	requires java.desktop;
-}
